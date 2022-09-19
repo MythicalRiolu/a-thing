80d8d27bb83862587413d748769cf06fa3318755
Riolu <102733938+MythicalRiolu@users.noreply.github.com>
2022-09-19 23:27:16 +0100

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,28 @@
+import random
+
+bool == False
+
+computer = random.randint(1,3) ##1 is rock, 2 is paper, 3 is scissors
+
+chosen = input(int("choose"))
+while bool == False:
+	if chosen = 1 and computer = 1:
+		print("draw")
+	elif chosen = 1 and computer = 2:
+		print("computer wins")
+	elif chosen = 1 and computer = 3:
+		print("player wins)
+	elif chosen = 2 and computer = 1:
+		print("player wins")
+	elif chosen = 2 and computer = 2:
+		print("draw")
+	elif chosen = 2 and computer = 3:
+		print("computer wins")
+	elif chosen = 3 and computer = 1:
+		print("computer wins")
+	elif chosen = 3 and computer = 2:
+		print("player wins")
+	elif chosen = 3 and computer = 3:
+		print("draw")
+		
+	else 
